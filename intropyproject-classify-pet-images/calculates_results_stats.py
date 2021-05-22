735b4ac6cddb15fb50421f94109688fb3f10cbde
Teresa Dąbrowska <84614120+tdabrowska@users.noreply.github.com>
2021-05-22 23:34:48 +0200

    Update calculates_results_stats.py
    Replacing with code from the previously finished project in udacity workspace

--- intropyproject-classify-pet-images/calculates_results_stats.py
+++ intropyproject-classify-pet-images/calculates_results_stats.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/calculates_results_stats.py
 #                                                                             
-# PROGRAMMER:
-# DATE CREATED:                                  
+# PROGRAMMER: Teresa Dabrowska
+# DATE CREATED: 05.01.2021                                  
 # REVISED DATE: 
 # PURPOSE: Create a function calculates_results_stats that calculates the 
 #          statistics of the results of the programrun using the classifier's model 
@@ -42,7 +42,44 @@
 #       in the return statement with the results_stats_dic dictionary that you create 
 #       with this function
 # 
-def calculates_results_stats(results_dic):
+def calculates_results_stats(results_dict):
+    results_stats_dic = {}
+    results_stats_dic['n_images'] = len(results_dict)
+    dogs_img_count = 0
+    notdogs_img_count = 0
+    match_count = 0
+    correct_dogs_count = 0
+    correct_notdogs_count = 0
+    correct_breed_count = 0
+    for key, value in results_dict.items():
+        if value[3] == 1:
+            dogs_img_count += 1
+        else:
+            notdogs_img_count += 1         
+        if value[2] == 1:
+            match_count += 1
+        if value[3] == 1 and value[4] == 1:
+            correct_dogs_count += 1
+        elif value[3] == 0 and value[4] == 0:
+            correct_notdogs_count +=1
+        if value[2] == 1 and value[3] == 1:
+            correct_breed_count += 1
+    results_stats_dic['n_dogs_img'] =  dogs_img_count 
+    results_stats_dic['n_notdogs_img'] =  notdogs_img_count  
+    results_stats_dic['n_match'] =  match_count
+    results_stats_dic['n_correct_dogs'] =  correct_dogs_count
+    results_stats_dic['n_correct_notdogs'] =  correct_notdogs_count
+    results_stats_dic['n_correct_breed'] =  correct_breed_count
+    results_stats_dic['pct_match'] =  (match_count / len(results_dict)) * 100
+    results_stats_dic['pct_correct_dogs'] =  (correct_dogs_count / dogs_img_count) * 100
+    results_stats_dic['pct_correct_breed'] =  (correct_breed_count / dogs_img_count) * 100
+    results_stats_dic['pct_correct_notdogs'] =  (correct_notdogs_count / notdogs_img_count) * 100
+
+    return results_stats_dic     
+            
+            
+        
+            
     """
     Calculates statistics of the results of the program run using classifier's model 
     architecture to classifying pet images. Then puts the results statistics in a 
@@ -65,9 +102,8 @@ def calculates_results_stats(results_dic):
                     a percentage or a count) where the key is the statistic's 
                      name (starting with 'pct' for percentage or 'n' for count)
                      and the value is the statistic's value. See comments above
-                     and the classroom Item XX Calculating Results for details
+                     and the previous topic Calculating Results in the class for details
                      on how to calculate the counts and statistics.
     """        
     # Replace None with the results_stats_dic dictionary that you created with 
     # this function 
-    return None
