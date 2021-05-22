49ed05341c9e18bb23240be74e4e071220416cc5
Teresa Dąbrowska <84614120+tdabrowska@users.noreply.github.com>
2021-05-22 23:30:13 +0200

    Update get_pet_labels.py
    Replacing with code from the previously finished project in udacity workspace

--- intropyproject-classify-pet-images/get_pet_labels.py
+++ intropyproject-classify-pet-images/get_pet_labels.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
+# PROGRAMMER: Teresa Dabrowska
+# DATE CREATED: 04.01.2021                                 
 # REVISED DATE: 
 # PURPOSE: Create the function get_pet_labels that creates the pet labels from 
 #          the image's filename. This function inputs: 
@@ -40,6 +40,42 @@ def get_pet_labels(image_dir):
       List. The list contains for following item:
          index 0 = pet image label (string)
     """
-    # Replace None with the results_dic dictionary that you created with this
+#     filename_list = listdir('pet_images/')
+    filename_list = listdir(image_dir)
+    label_list = []
+    for i in filename_list:
+        if filename_list[0] != ".":
+            label_splitted = i.split('_')
+            label = ""
+            for word in label_splitted:
+                if word.isalpha():
+                    label += word + " "
+            label = label.lower().strip()
+            label_list.append(label)
+#     print(label_list)
+#     print(filename_list)
+#     print(len(filename_list))
+#     print("\n Prints 10 filenames from folder pet_images/")
+#     for idx in range(0, 10, 1):
+#           print("{:2d} file: {:25}".format(idx + 1, filename_list[idx]))
+          
+    
+#     Replace None with the results_dic dictionary that you created with this
     # function
-    return None
+    results_dict = {}
+    items_in_dict = len(results_dict)
+    print('\nEmpty Dictionary results_dict - n items=', items_in_dict)
+    
+    for idx in range(0, len(filename_list)):
+        if filename_list[idx] not in results_dict:
+           results_dict[filename_list[idx]] = [label_list[idx]]
+        else: 
+            print("**Warning: Key=", filename_list[idx],
+                  "already exists in results_dict with value=",
+                  results_dict[filename_list[idx]])
+                    
+    print("\n Printing all key-value pairs in dictionary results_dict:")              
+    for key in results_dict:
+        print("Filename=", key, "   Pet Label=", results_dict[key][0])
+                    
+    return results_dict
